15b734403ca4de0bef969e48eb674b67f3c7e18c
iryna <irakirvas78@gmail.com>
2026-02-18 21:20:54 +0200

    feat: initialize users app and define custom User model

--- library/settings.py
+++ library/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "rest_framework",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -97,6 +99,9 @@ AUTH_PASSWORD_VALIDATORS = [
         "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
+
+
+AUTH_USER_MODEL = 'users.User'
 
 
 # Internationalization
