d35d81845d9150dd48799bb066f7456d3a93d4c0
iryna <irakirvas78@gmail.com>
2026-02-21 18:53:39 +0200

    feat: add BorrowingCreateSerializer with inventory validation & atomic transaction

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from books.serializers import BookSerializer
 from borrowings.models import Borrowing
@@ -17,3 +18,26 @@ class BorrowingReadSerializer(serializers.ModelSerializer):
             "book",
             "user",
         )
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "book",
+            "expected_return_date",
+        )
+
+    def validate(self, attrs):
+        book = attrs["book"]
+        if book.inventory <= 0:
+            raise serializers.ValidationError("This book is out of stock.")
+        return attrs
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        with transaction.atomic():
+            book.inventory -= 1
+            book.save()
+            return super().create(validated_data)
