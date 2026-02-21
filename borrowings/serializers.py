65b55df2b6d7b277a0994abe230e40fab1f76f79
iryna <irakirvas78@gmail.com>
2026-02-21 18:15:31 +0200

    feat: implement borrowing list & detail endpoints with detailed book info

---
+++ borrowings/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from books.serializers import BookSerializer
+from borrowings.models import Borrowing
+
+
+class BorrowingReadSerializer(serializers.ModelSerializer):
+    book = BookSerializer(read_only=True)
+    user = serializers.SlugRelatedField(read_only=True, slug_field="email")
+
+    class Meta:
+        model = Borrowing
+        fields = (
+            "id",
+            "borrow_date",
+            "expected_return_date",
+            "actual_return_date",
+            "book",
+            "user",
+        )
